4a5e39161190745c307cd1477282ae972b30d3f5
Shikhar2705 <shikhar.saxena2705@gmail.com>
2025-09-15 23:55:24 +0530

    Complete Server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('✅ Nokasa monitoring service is running.');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
